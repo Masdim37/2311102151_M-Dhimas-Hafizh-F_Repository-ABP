26cbdca9f5e3c347bcbffef8a2e610ad571ba7cb
Masdim37 <dhimas.hfzh375@gmail.com>
2026-03-09 21:31:46 +0700

    push tugas 2 javascript

--- Pertemuan-2/Tugas 2 Javascript/Soal Nomor 2/TUTORIAL_JS_2311102151_Muhammad Dhimas Hafizh Fathurrahman_No 2.js
+++ Pertemuan-2/Tugas 2 Javascript/Soal Nomor 2/TUTORIAL_JS_2311102151_Muhammad Dhimas Hafizh Fathurrahman_No 2.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
-
     $("#btnAlign").click(function(){
-
         var align = $(".content-row").css("text-align");
-
         if(align == "left"){
             $(".content-row").css("text-align","center");
         }
@@ -13,14 +10,9 @@ $(document).ready(function(){
         else{
             $(".content-row").css("text-align","left");
         }
-
     });
-
 
     $("#btnBg").click(function(){
-
         $(".box").toggleClass("bg-danger");
-
     });
-
 });
