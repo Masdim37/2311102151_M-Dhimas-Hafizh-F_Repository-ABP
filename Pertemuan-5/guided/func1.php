26dee3e24659982a4b5df13cc94663904c120520
Masdim37 <dhimas.hfzh375@gmail.com>
2026-04-11 12:20:07 +0700

    up laprak pertenuan 5

---
+++ Pertemuan-5/guided/func1.php
@@ -0,0 +1,12 @@
+<?php
+function cetakGenap() {
+    for ($i = 1; $i <= 100; $i++) {
+        if ($i % 2 == 0) {
+            echo "$i ";
+        }
+    }
+}
+
+//pemanggilan fungsi
+cetakGenap();
+?>
---
+++ Pertemuan-5/guided/func3.php
@@ -0,0 +1,10 @@
+<?php 
+function luasSegitiga($alas, $tinggi) {
+    return 0.5 * $alas * $tinggi;
+}
+
+//pemanggilan fungsi
+$a = 10;
+$t = 50;
+echo "Luas Segitiga dengan alas $a dan tinggi $t adalah : " . luasSegitiga($a, $t);
+?>
